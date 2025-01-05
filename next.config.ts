82eb6b594e824bcd61c3ad5ddd973bc6c649da33
dorakemon <51844896+dorakemon@users.noreply.github.com>
2025-01-06 01:31:31 +0900

    fix: remove base dir settings

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const SUB_DIRECTORY = "/vc-with-usb-key-playground";
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? SUB_DIRECTORY : "",
 };
 
 export default nextConfig;
