ed593e4bd6fb2f379e8b4ee2e23fb18900543c74
dorakemon <51844896+dorakemon@users.noreply.github.com>
2025-01-05 11:39:56 +0900

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { geistMono, geistSans } from "@/styles/fonts";
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "VC Holder Binding Playground",
+  description: "VC Holder Binding Playground",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
